b0e4bdf30f7698bb85c5a52cb55402f4d644b36f
moyisuosheng <moyisuosheng@outlook.com>
2025-03-23 01:52:39 +0800

    🎈 perf(代理管理): 实现单实例应用程序支持
    在 `App.xaml.cs` 中添加了对 `System.Diagnostics` 的引用。引入了静态可空 `Mutex` 变量 `_mutex`，确保应用程序的单实例运行。构造函数中创建 `Mutex` 实例并检查是否已有实例在运行，若是则弹出提示并退出。新增 `CreateMauiApp` 方法以创建 `MauiApp` 实例，并重写 `Exit` 方法以释放互斥体资源并记录调试信息。

--- ProxyManage/Platforms/Windows/App.xaml.cs
+++ ProxyManage/Platforms/Windows/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using ProxyManage.Platforms.Windows;
+using System.Diagnostics;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -10,12 +11,28 @@ namespace ProxyManage.WinUI;
 /// </summary>
 public partial class App : MauiWinUIApplication
 {
+
+    private static Mutex? _mutex = null;
+
     /// <summary>
     /// Initializes the singleton application object.  This is the first line of authored code
     /// executed, and as such is the logical equivalent of main() or WinMain().
     /// </summary>
     public App()
     {
+        // 确保应用程序的单实例运行
+        const string appName = "ProxyManageApp";
+
+        _mutex = new Mutex(true, appName, out bool createdNew);
+
+        if (!createdNew)
+        {
+            // 如果已经有一个实例在运行，弹出提示框
+            MessageUtil.ShowSuccessMessage("已经有一个 ProxyManage 程序在运行！");
+            // 退出应用程序
+            Environment.Exit(0);
+        }
+
         this.InitializeComponent();
 
         // 注册服务依赖
@@ -23,6 +40,34 @@ public partial class App : MauiWinUIApplication
         DependencyService.Register<IProxyUtil, ProxyUtil>();
         DependencyService.Register<IFolderOpener, FolderOpener>();
     }
+
+    /// <summary>
+    /// 创建 MauiApp
+    /// </summary>
+    /// <returns></returns>
     protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+
+    /// <summary>
+    /// 释放资源
+    /// </summary>
+    public new void Exit()
+    {
+        try
+        {
+            // 释放互斥体
+            _mutex?.ReleaseMutex();
+            _mutex = null;
+            Debug.WriteLine("释放资源");
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex.Message);
+        }
+        finally
+        {
+            base.Exit();
+        }
+    }
+
 }
 
